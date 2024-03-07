2ffe3662b08ce47ff44b0c616ce66aa0644fb5b2
Jenny Munar <jennymunar@jennys-mbp.lan>
2024-03-06 21:23:03 -0600

    added comments to my files

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+// to import the modules
 const inquirer = require('inquirer');
 const {writeFile} = require('fs/promises');
 const { Triangle, Circle, Square } = require('./lib/shapes.js');
 const SVG = require('./lib/svg.js');
+
+//array of questioons for the user
 const questions = [
         {
             type: 'input',
             message: 'Type the text for your code up to three characters:',
             name: 'text',
-            default: 'svg'
+            //default value for the text so user can get an idea
+            default: 'svg' 
         },
         {
             type: 'input',
@@ -27,6 +31,7 @@ const questions = [
         },
     ];
 
+//take the chosend shape and creates it
     inquirer.prompt(questions).then(({text, textColor, shape, shapeColor}) => {
           switch (shape[0]) {
           case 'Triangle':
@@ -41,10 +46,13 @@ const questions = [
         
         }
       shape.setColor(shapeColor)
+
+      //to create a new svg object
       const svg = new SVG()
       svg.setText(text, textColor)
       svg.setShape(shape)
       return writeFile("./examples/logo.svg", svg.render())
       
       })
+      //to log success mesage after the file is generated
       .then(() => console.log("Generated logo.svg"))
